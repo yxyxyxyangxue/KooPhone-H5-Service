ee061b60094ba756cb30370db50f521d2c2be019
Olive-001 <zhangjh_123@outlook.com>
2023-05-23 21:41:45 +0800

    提交测试

---
+++ src/main/java/com/huawei/apaas/koophone/freetraffic/FreeTrafficApplication.java
@@ -0,0 +1,18 @@
+package com.huawei.apaas.koophone.freetraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author zhangjihong
+ * @since 2023-05-23
+ */
+@SpringBootApplication
+public class FreeTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huawei/apaas/koophone/freetraffic/FreeTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huawei.apaas.koophone.freetraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FreeTrafficApplicationTests {
+// config.RestTemplateConfigTest
+	@Test
+	void contextLoads() {
+	}
+
+}
